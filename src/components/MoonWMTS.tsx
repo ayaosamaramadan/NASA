6e57613062c943896278071fb6da620657a87873
ayaosamaramadan <ayaramadan2011@gmail.com>
2026-01-05 21:24:45 +0200

    add MoonMap with Leaflet int

--- src/components/MoonWMTS.tsx
+++ src/components/MoonWMTS.tsx
@@ -1,8 +1,34 @@
-const MoonWMTS = () => {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-black via-gray-900 to-black text-white p-4">
-            </div>
-    );
-}
 
-export default MoonWMTS;
+import { MapContainer, TileLayer } from "react-leaflet";
+import * as L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MoonMap = () => {
+  return (
+    <MapContainer
+      center={[-150, 20]}  
+         zoom={3}      
+      style={{ height: "100vh", width: "100%" }}
+      minZoom={2}
+      maxZoom={6}
+      zoomControl={false}
+      crs={L.CRS.Simple}
+      >
+    <div
+        className="leaflet-control-zoom leaflet-bar"
+        style={{ position: "absolute", top: 10, left: 10, zIndex: 1000 }}
+    >
+        <button className="z-50 leaflet-control-zoom-in">+</button>
+        <button className="z-50 leaflet-control-zoom-out">−</button>
+    </div>
+      <TileLayer className="-z-50"
+        url="https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://nasa.gov">NASA</a>'
+        tms={false}
+        noWrap={true}
+      />
+    </MapContainer>
+  );
+};
+
+export default MoonMap;
